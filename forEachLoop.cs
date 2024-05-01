27f6adaaee2292c218199be2595c2446b50d4af2
Mehir_007 <mehirrouth20000@gmail.com>
2024-05-01 16:00:38 +0530

    ForEach Loop

---
+++ forEachLoop.cs
@@ -0,0 +1,13 @@
+using System;
+
+class Demo
+{
+    public static void Main()
+    {
+        int[] nums = { 1, 2, 3, 4, 5, 6, 7, 8 };
+        foreach (int x in nums)
+        {
+            Console.WriteLine(x);
+        }
+    }
+}
